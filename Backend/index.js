e96ced9b579a66243da1682a5a4550da7d2515f6
Yan Silva <yanbarbosa19@gmail.com>
2021-02-12 18:08:48 -0300

    config routes

--- Backend/index.js
+++ Backend/index.js
@@ -2,8 +2,10 @@ const app = require('express')()
 const cors = require('cors')
 const PORT = 9001
 const HOST = '0.0.0.0'
+const { auth } = require('./routes')
 
 app.use(cors())
+app.use('/auth', auth)
 app.listen(PORT, HOST, () => {
     console.log('Listening PORT 9001')
 })
