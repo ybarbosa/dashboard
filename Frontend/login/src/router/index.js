960431f27b6d0bc388b5a08628fb9478550425b1
Yan Silva <yanbarbosa19@gmail.com>
2021-02-12 16:08:16 -0300

    added and settings router

---
+++ Frontend/login/src/router/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Components from '../components'
+
+Vue.use(VueRouter)
+
+const router = new VueRouter ({
+    mode: 'hash',
+    routes: [
+        ...Components
+    ]
+})
+
+router.afterEach((to) => {
+    Vue.nextTick(() => {
+        document.title = to.meta.title
+    })
+})
+
+export default router
