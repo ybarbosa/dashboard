b46e9814e4eaaf737c29e737f9f82c1a5e144d96
Yan Silva <yanbarbosa19@gmail.com>
2021-02-13 12:13:27 -0300

    method create user

--- Backend/routes/users/create.js
+++ Backend/routes/users/create.js
@@ -1,6 +1,31 @@
+const { validationResult } = require('express-validator')
+const db = require('../../db/infrastructure')
+const dbName = 'users'
+const crypt = require('../../plugins/bcrypt')
+const timeStamp = require('../../plugins/timeStamp')
 
-const create = async (_, res) => {
-    return res.send({ 'teste': 'rota funcionando'})
+const create = async (req, res) => {
+    try {
+        const { password, birthday, email, name } = req.body
+        const error = await validationResult(req)
+        if (!error.isEmpty()) {
+            const erros = error.array().map(({ msg, param}) => ({ msg, param }))
+            return res.status(422).send( erros )
+        }
+        const collection = await db(dbName)
+        const modelUser = {
+            name,
+            email,
+            password: await crypt(password),
+            birthday: timeStamp(birthday, 'YYYY/mm/DD').format('LL'),
+            createdAt: timeStamp().format('LL')
+        }
+
+        await collection.insertOne(modelUser)
+        return res.status(201).send( modelUser )
+    } catch (err) {
+        return res.status(404).send(err)
+    }
 }
 
 module.exports = create
