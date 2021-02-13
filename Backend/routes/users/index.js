4d1787c697c079143d62e9d069e247202c148dd0
Yan Silva <yanbarbosa19@gmail.com>
2021-02-13 12:14:04 -0300

    validators create user

--- Backend/routes/users/index.js
+++ Backend/routes/users/index.js
@@ -1,6 +1,16 @@
 const router = require('../router')
 const create = require('./create')
+const get = require('./get')
+const { body } = require('express-validator')
 
-router.get('/create', create)
+const validators = [
+    body('email').isEmail(),
+    body('name').isString(),
+    body('password').isString().isLength({ min: 3 }),
+    body('birthday').isDate("YYYY/MM/DD"),
+]
+
+router.get('/', get)
+router.post('/create', validators , create)
 
 module.exports = router
