181374d1f8b0a0d7bc3fa9b091eea07f1d7fd1dc
Yan Silva <yanbarbosa19@gmail.com>
2021-02-13 12:13:15 -0300

    fix body

--- Backend/index.js
+++ Backend/index.js
@@ -1,9 +1,16 @@
 const app = require('express')()
 const cors = require('cors')
+const bodyParser = require('body-parser')
+
 const PORT = 9001
 const HOST = '0.0.0.0'
+
 const { auth, users } = require('./routes')
 
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+app.use(bodyParser.json())
 app.use(cors())
 app.use('/auth', auth)
 app.use('/user', users)
