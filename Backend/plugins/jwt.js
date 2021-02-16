f7140cbd10dafdab7dea850ac5057303cd730d89
Yan Silva <yanbarbosa19@gmail.com>
2021-02-16 14:02:00 -0300

    fix jwt

--- Backend/plugins/jwt.js
+++ Backend/plugins/jwt.js
@@ -1,10 +1,25 @@
 const jwt = require('jsonwebtoken')
 
 const createUserToken = (user) => {
-	return jwt.sign({ id : user }, 'xuxinha', {expiresIn: '7d'} )
+	return jwt.sign({ id : user }, 'xuxinha', { expiresIn: 1 })
+}
+
+const auth = async (req, res, next) => {
+    try {
+        const tokenHeader = req.headers.auth
+        if(!tokenHeader) return res.status(401).send( { error: 'token not sent' } )
+        
+        const token = await jwt.verify( tokenHeader, 'xuxinha')
+        req.token = token
+        return next()
+    } catch (error) {
+        //  
+        return res.status(401).send( { error: error.message } )
+    }
 }
 
 module.exports = {
     createUserToken,
+    auth,
     jwt
 }
