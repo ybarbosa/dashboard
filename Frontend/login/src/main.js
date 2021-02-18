d51f4245efab230e0496a65dd6c1608e3049d7a6
Yan Silva <yanbarbosa19@gmail.com>
2021-02-18 11:53:24 -0300

    adding new properties to the vue instance

--- Frontend/login/src/main.js
+++ Frontend/login/src/main.js
@@ -2,7 +2,23 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import router from './router'
+import VeeValidate, { Validator } from 'vee-validate'
+import dictionary from './plugins/dictionary'
+import VueTheMask from 'vue-the-mask'
+import http from './plugins/http'
+
 Vue.config.productionTip = false
+
+
+Validator.localize(dictionary)
+Vue.use(VeeValidate, { 
+  locale: 'pt_BR',
+  enableAutoClasses: true,
+  fieldsBagName: 'vvFields'
+})
+
+Vue.use(VueTheMask)
+Vue.use(http)
 
 new Vue({
   vuetify,
