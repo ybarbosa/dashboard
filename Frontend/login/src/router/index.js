849d214cf08a3db917af1432778eaf0d704906ed
Yan Silva <yanbarbosa19@gmail.com>
2021-02-21 15:39:39 -0300

    rename file

--- Frontend/login/src/router/index.js
+++ Frontend/login/src/router/index.js
@@ -1,13 +1,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Components from '../views'
+import Views from '../views'
 
 Vue.use(VueRouter)
 
 const router = new VueRouter ({
     mode: 'hash',
     routes: [
-        ...Components
+        ...Views
     ]
 })
 
