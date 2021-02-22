f2ee377bc78c4431345af6404888fb28ec073ad9
Yan Silva <yanbarbosa19@gmail.com>
2021-02-22 11:16:11 -0300

    added vuetify

--- Frontend/dashboard/src/main.js
+++ Frontend/dashboard/src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuex from 'vuex'
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
+Vue.use(Vuex)
 
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
